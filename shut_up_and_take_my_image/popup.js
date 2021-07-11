7c7ef7c241978ed0edd2e6b79cb6330c04e56b05
N0tail12 <62517449+N0tail12@users.noreply.github.com>
2021-07-11 22:26:00 +0700

    Add files via upload

--- shut_up_and_take_my_image/popup.js
+++ shut_up_and_take_my_image/popup.js
@@ -3,11 +3,12 @@
   let image_List = document.getElementById("list_image");
   let downloadImages = document.getElementById("download_button");
   let count = 0;
+  let test;
   const getName = (url) => {
     const path = url.split("/");
     return path[path.length - 1];
   };
-
+ // create element with class and config
   const createElement = (el, class_name, config = {}) => {
     const element = document.createElement(el);
     element.className = class_name || "";
@@ -16,13 +17,13 @@
     }
     return element;
   };
-
+// create list of image
   const createList = (src) => {
     const list = createElement("div", "image_grid");
     const link = createElement("a", undefined, {
       href: src,
       target: "_blank",
-      download: getName(src),
+      download: "",
     });
     const preview = createElement("img", undefined, { src });
     // console.log(link);
@@ -35,7 +36,7 @@
     // '</a>' +
     // '</div>';
   };
-
+// render list of image
   const renderList = (images, el) => {
     el.innerHTML = "";
     const create_List = createElement("div", "list");
@@ -63,71 +64,131 @@
     el.appendChild(create_List);
   };
 
+
+//get images on page
   const getPageImages = () => {
     // const images = document.body.getElementsByTagName("img");
     // console.log('images: ',images);
     // const imageSource = Array.from(images).map((el) => el.src);
     // return imageSource;
-    var images = [], bgImg, src;
-    const getUrls = (el) =>{
-        bgImg = el.style.backgroundImage;
-        if(bgImg){
-            src = bgImg.replace(/^\s?url\((\'|\")/,'').replace(/(\"|\')\)\s?$/, '');
-            bgImg = '';
-        }else{
-            src = el.src;
-        }
-        if(src.match(/^data\:image|\/\//)){
-            return src;
-        }
-        return document.location.origin.replace(/\/$/, '') + '/' + src;
-    }
-    const reduceArray = (a,b) => {
-        if(a.indexOf(b) < 0){
-            a.push(b);
-        }
-        return a;
-    }
-    var img_urls = Array.prototype.map.call(document.querySelectorAll('img, [style*="background-image"]'), getUrls).reduce(reduceArray, []);
+    var images = [],
+      bgImg,
+      src;
+    const getUrls = (el) => {
+      bgImg = el.style.backgroundImage;
+      if (bgImg) { // if the image is background.;
+        src = bgImg
+          .replace(/^\s?url\((\'|\")/, "")
+          .replace(/(\"|\')\)\s?$/, "");
+        bgImg = "";
+      } else {
+        src = el.src;
+      }
+      if (src.match(/^data\:image|\/\//)) { // if source is data:image type or contains "//";
+        return src;
+      }
+      return document.location.origin.replace(/\/$/, "") + "/" + src; // if source other type. for example "/img/img.png";
+    };
+
+    const reduceArray = (a, b) => { // if 2 images are the same source;
+      if (a.indexOf(b) < 0) {
+        a.push(b);
+      }
+      return a;
+    };
+
+    var img_urls = Array.prototype.map
+      .call(
+        document.querySelectorAll('img, [style*="background-image"]'),
+        getUrls
+      )
+      .reduce(reduceArray, []);
     return img_urls;
   };
 
+  // download
   const downloadAll = () => {
     const filter = images.filter((item) => getName(item) !== "");
     filter.forEach((item) => {
-      const link = createElement("a", undefined, {
-        href: item,
-        download: getName(item),
-      });
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+      // chrome.downloads.download({
+      //   url: item,
+      // });
+      downloadResource(item);
+      // const link = createElement("a", undefined, {
+      //   href: item,
+      //   download: getName(item),
+      // });
+      // document.body.appendChild(link);
+      // link.click();
+      // document.body.removeChild(link);
     });
   };
-
+  // handle download
   const handleDownload = () => {
-    if (window.confirm(`Will be downloaded ${count} files. Are you sure?`))
-      downloadAll();
-  };
-  chrome.tabs.query({ active: true }, function (tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          function: getPageImages,
-        },
-        (injectionResults) => {
-          images = injectionResults[0].result;
-          console.log(images);
-          if (images.length) {
-            downloadImages.style.display = "block";
-            downloadImages.onclick = handleDownload;
-          }
-          renderList(injectionResults[0].result, image_List);
-        }
-      );
+    if (window.confirm(`Will be downloaded ${count} files. Are you sure?`)){
+      // let length = Math.ceil(count / 10);
+      // let start = 0;
+      // let end_arr = 10;
+      // for (let i = 0; i < length; ++i) {
+      //   if(end_arr >= count){
+      //     end_arr = count;
+      //   }
+      //   test = images.slice(start, end_arr);
+        downloadAll();
+      //   console.log('img2', images);
+      //   console.log('test', test);
+      //   start = start + 10;
+      //   end_arr = end_arr + 10;
+      // }
+      
     }
-  });
-    // images = getPageImages();
-    // console.log(images);
+  };
+
+
+  // force to download image by change header to allow cross-origin
+  function forceDownload(blob, filename) {
+    var a = document.createElement("a");
+    a.download = filename;
+    a.href = blob;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
+  function downloadResource(url, filename) {
+    if (!filename) filename = url.split("\\").pop().split("/").pop();
+    fetch(url, {
+      headers: new Headers({
+        Origin: location.origin,
+      }),
+      mode: "cors",
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        let blobUrl = window.URL.createObjectURL(blob);
+        forceDownload(blobUrl, filename);
+      })
+      .catch((e) => console.error(e));
+  }
+// execute Script;
+  window.onload = function () {
+    chrome.tabs.query({ active: true }, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabs[0].id },
+            function: getPageImages,
+          },
+          (injectionResults) => {
+            images = injectionResults[0].result;
+            console.log('img1', images);
+            if (images.length) {
+              downloadImages.style.display = "block";
+              downloadImages.onclick = handleDownload;
+              renderList(images, image_List);
+            }
+          }
+        );
+      }
+    });
+  };
 })();
